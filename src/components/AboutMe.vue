7ea002495d2d0d9d9652ccaf26527a0555fcb4f6
lbjcena <lbjcena@gmail.com>
2025-08-13 01:23:39 +0200

    additional tools added to aboutme section

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -5,27 +5,51 @@
       <p>I specialize in front-end develoment of websites and web apps, focusing on delivering elegant, scalable and
         performant designs, fully compliant with modern web development practices.</p>
       <div class="tech-stack flex flex-col gap-10">
-        <h3>Tech stack I work with :</h3>
+        <h3>Main tech stack I use :</h3>
         <ul class="grid grid-cols-2 grid-rows-2 desktop:grid-cols-4 desktop:grid-rows-1 gap-5 w-full justify-around">
-          <li><img width="48" src="/icons/vue-js.svg" alt="Vue.js official logo"><a target="_blank"
-              href="https://vuejs.org/">
+          <li><a target="_blank" href="https://vuejs.org/">
+              <img width="48" src="/icons/vue-js.svg" alt="Vue.js official logo">
               <h4>Vue.js</h4>
               <span>Front-end implementation</span>
             </a>
           </li>
-          <li><img width="48" src="/icons/vite.svg" alt="Vite official logo"><a target="_blank"
-              href="https://vite.dev/">
+          <li><a target="_blank" href="https://vite.dev/">
+              <img width="48" src="/icons/vite.svg" alt="Vite official logo">
               <h4>Vite</h4><span>Build tool & bundler</span>
             </a></li>
-          <li><img width="48" src="/icons/typescript.svg" alt="TypeScript official logo"><a target="_blank"
-              href="https://www.typescriptlang.org/">
+          <li><a target="_blank" href="https://www.typescriptlang.org/">
+              <img width="48" src="/icons/typescript.svg" alt="TypeScript official logo">
               <h4>TypeScript</h4><span>Type checking and error prevention</span>
             </a></li>
-          <li><img width="48" src="/icons/tailwindcss.svg" alt="Tailwind official logo"><a target="_blank"
-              href="https://tailwindcss.com/">
+          <li><a target="_blank" href="https://tailwindcss.com/">
+              <img width="48" src="/icons/tailwindcss.svg" alt="Tailwind official logo">
               <h4>Tailwind CSS</h4>
               <span>Quick and modular CSS styling</span>
             </a></li>
+        </ul>
+        <h3>Additional tools :</h3>
+        <ul class="grid grid-cols-2 grid-rows-2 desktop:grid-cols-3 desktop:grid-rows-1 gap-5 w-full justify-around">
+          <li>
+            <a target="_blank" href="https://nodejs.org/">
+              <img src="/icons/nodejs.svg" width="48" alt="Node.js official logo">
+              <h4>Node.js</h4>
+              <span>Backend integrations and features</span>
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://vercel.com">
+              <img src="/icons/vercel.webp" alt="Vercel official logo">
+              <h4>Vercel</h4>
+              <span>Hosting</span>
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://github.com">
+              <img src="/icons/github.svg" alt="">
+              <h4>GitHub</h4>
+              <span>Version control</span>
+            </a>
+          </li>
         </ul>
         <small>Icons from: <a class="text-primary font-medium hover:underline"
             href="https://www.icons8.com">Icons8.com</a></small>
@@ -44,7 +68,7 @@
   @apply animate-wiggle;
 }
 .tech-stack li a {
-  @apply w-full block text-center;
+  @apply w-full flex flex-col items-center text-center;
 }
 
 .tech-stack h4 {
