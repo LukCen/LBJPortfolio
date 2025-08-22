3d1eb22f40bb05e328e1d943360c209b722f62f8
lbjcena <lbjcena@gmail.com>
2025-08-22 18:53:51 +0200

    ditto - optimization attempts, returned to preloading css, removed prealoding fonts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  build: {
-    cssCodeSplit: false
-  },
+  plugins: [vue(), tailwindcss()]
 
 })
