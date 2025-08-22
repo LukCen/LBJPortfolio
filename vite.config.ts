8ee809f873eed2ffcb639fecd3f52cbf4fdeff80
lbjcena <lbjcena@gmail.com>
2025-08-22 19:39:42 +0200

    beasties removed, FOUC looks worse and Lighthouse complains

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import { beasties } from 'vite-plugin-beasties'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), beasties({
-    options: {
-      preload: 'swap',
-      pruneSource: false,
-      inlineThreshold: 4000
-    },
-    filter: path => path.endsWith('.html')
-  })]
+  plugins: [vue(), tailwindcss()]
 })
