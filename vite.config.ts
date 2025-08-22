d5b7955d70c59878f2d862aec5cc8563f8af8651
lbjcena <lbjcena@gmail.com>
2025-08-22 19:27:16 +0200

    beasties + vite plugin added, further attempts at optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-
+import { beasties } from 'vite-plugin-beasties'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  build: {
-    cssCodeSplit: false
-  },
-
+  plugins: [vue(), tailwindcss(), beasties({
+    options: {
+      preload: 'swap',
+      pruneSource: true,
+      inlineThreshold: 4000
+    },
+    filter: path => path.endsWith('.html')
+  })]
 })
