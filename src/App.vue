d130a3c7b861e8f5f4cadb1bb6f7308e8b0b51d4
lbjcena <lbjcena@gmail.com>
2026-02-04 23:28:48 +0100

    old comments removed

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,6 @@ import ContactMe from './components/ContactMe.vue';
 </script>
 
 <template>
-  <!-- <Navbar />
-  <main class="flex flex-col gap-20 w-screen desktop:max-w-[1200px] px-2 desktop:px-4 items-center mx-auto mb-2">
-    <HeroSection />
-    <AboutMe />
-    <Portfolio />
-    <ContactMe />
-  </main> -->
   <div class="layout">
     <Navbar class="full" />
     <HeroSection class="full" />
